754d38db9da7f76c4b312ec94829d2a6d6154c51
rimomin <rimomin@localhost>
2026-05-10 16:59:23 +0900

    RomajiHiraganaPairMaker
    RomajiHiraganaPairMaker now can generate orderd array for RomajiToHiraganaLibrary from RomajiHiraganaPairs.xml

--- RomajiHiraganaPairMaker/Program.cs
+++ RomajiHiraganaPairMaker/Program.cs
@@ -8,10 +8,25 @@ namespace RomajiHiraganaPairMaker
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            const string xmlFilePath = @"RomajiHiraganaPairs.xml";
             const string replaceContentFormat = "new KeyValuePair<string, string>(\"{0}\",\"{1}\"),";
+
+            Dictionary<string, string> parsedArgs = ArgParse(args);
+
+            string xmlFilePath; // RomajiHiraganaPairs.xml
+            string sourceFilePath; // RomajiToHiragana.cs
+            try
+            {
+                xmlFilePath = parsedArgs["dict"];
+                sourceFilePath = parsedArgs["out"];
+            }
+            catch (KeyNotFoundException)
+            {
+                Console.Error.WriteLine("Fatal: Required option(s) not found.");
+                ShowHelp();
+                return -1;
+            }
 
             List<RomajiHiraganaPair> romajiHiraganaPairs = LoadRomajiHiraganaPair(xmlFilePath);
             List<string> replaceContents = new List<string>(romajiHiraganaPairs.Count);
@@ -21,7 +36,37 @@ namespace RomajiHiraganaPairMaker
                 string line = romajiHiraganaPair.ToString(replaceContentFormat);
                 replaceContents.Add(line);
             }
-            SourceGenerator("RomajiToHiragana.cs", replaceContents);
+            SourceGenerator(sourceFilePath, replaceContents);
+
+            return 0;
+        }
+
+        private static void ShowHelp()
+        {
+            string appName = Path.GetFileName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            Console.Error.WriteLine("Usages: {0} /dict xmlfile /out sourcefile", appName);
+            Console.Error.WriteLine("xmlfile   : xml dictionary path");
+            Console.Error.WriteLine("sourcefile: output source code file path");
+        }
+
+        private static Dictionary<string, string> ArgParse(string[] args)
+        {
+            string key = "";
+            var parsedArgs = new Dictionary<string, string>();
+
+            foreach (var arg in args)
+            {
+                if (arg[0] == '/')
+                {
+                    key = arg.Substring(1);
+                }
+                else if (key != "")
+                {
+                    parsedArgs.Add(key, arg);
+                }
+            }
+
+            return parsedArgs;
         }
 
         private static List<RomajiHiraganaPair> LoadRomajiHiraganaPair(string filePath)
@@ -36,7 +81,24 @@ namespace RomajiHiraganaPairMaker
                 romajiHiraganaPairs = (RomajiHiraganaPairs)xmlSerializer.Deserialize(fs);
             }
 
-            romajiHiraganaPairs.Contents.Sort((x, y) => (y.Key.Length - x.Key.Length));
+            // ローマ字の比較は小文字で行うため、すべて小文字に統一する。
+            foreach(var content in romajiHiraganaPairs.Contents){
+                content.Key = content.Key.ToLowerInvariant();
+            }
+
+            // 文字数の多い順に置換する必要がある。
+            romajiHiraganaPairs.Contents.Sort((x, y) => {
+                // .OrderByDescending(x => x.Key.Length
+                var diff = y.Key.Length - x.Key.Length;
+                if (diff != 0) return diff;
+                // .ThenBy(x => x.Romaji)
+                for (var i = 0; i < x.Key.Length; i++)
+                {
+                    diff = x.Key[i] - y.Key[i];
+                    if (diff != 0) return diff;
+                }
+                return 0;
+            });
 
             return romajiHiraganaPairs.Contents;
         }
@@ -49,80 +111,89 @@ namespace RomajiHiraganaPairMaker
 
         private static void SourceGenerator(string filePath, IEnumerable<string> replaceContents)
         {
+            const string regionStartMarker = "#region romajiHiraganaPairs";
+            const string regionEndMarker = "#endregion";  
+
             using (var fs = new FileStream(filePath, FileMode.Open, FileAccess.ReadWrite))
             {
-                StringBuilder fileContent = new StringBuilder();
-
-                const int bufferSize = 1024;
-                const string regionStartMarker = "#region romajiHiraganaPairs";
-                const string regionEndMarker = "#endregion";
-
-                byte[][] buffer = new byte[2][];
-                var encoding = Encoding.GetEncoding("UTF-8");
-
-                buffer[0] = new byte[bufferSize];
-                fs.Read(buffer[0], 0, bufferSize);
-                string partialText = encoding.GetString(buffer[0]);
-                int startpos = partialText.IndexOf(regionStartMarker);
-
-
-                int endpos = partialText.IndexOf(regionEndMarker);
-
-                
+                StringBuilder fileContent = new StringBuilder();         
 
                 var streamReader = new StreamReader(fs);
+
+                // 書き換える領域の開始まで読み取る。
                 var innerRegion = false;
                 var indentCount = 0;
-                long insertPosition = 0;
                 while (!streamReader.EndOfStream)
                 {
                     var line = streamReader.ReadLine();
-                    if (line == "#region romajiHiraganaPairs")
+                    fileContent.Append(line);
+                    fileContent.Append("\r\n");
+                    if (line == regionStartMarker)
                     {
-                        insertPosition = fs.Position;
                         innerRegion = true;
                         break;
                     }
-                    indentCount = 0;
+                    indentCount = 4;
                     foreach (var c in line)
                     {
                         if (c == ' ') indentCount++;
                         else break;
                     }
                 }
-                if (innerRegion)
+
+                // 書き換える領域が見つからなかった場合は何もせずに終了する。
+                if (!innerRegion) return;
+                
+                // 書き換え後の領域に書き込む予定の文字列を生成
+                string indent = "".PadLeft(indentCount, ' ');
+                bool hasChange = false;
+                foreach (var replaceContent in replaceContents)
                 {
-                    string indent = "".PadLeft(indentCount, ' ');
-                    foreach (var replaceContent in replaceContents)
+                    string newline = indent + replaceContent;
+
+                    // 書き換え前と同一かを確認
+                    // 書き換え後のデータがまだある段階でファイル終端に達するか、領域が終了した場合、変更ありとみなす。
+                    if (streamReader.EndOfStream || !innerRegion)
                     {
-                        fileContent.Append(indent);
-                        fileContent.Append(replaceContent);
-                        fileContent.Append('\n');
+                        hasChange = true;
                     }
+                    else if (!hasChange)
+                    {
+                        string oldline = streamReader.ReadLine();
+                        if (oldline != newline) hasChange = true;
+                        if (oldline == regionEndMarker) innerRegion = false;
+                    }
+
+                    fileContent.Append(indent);
+                    fileContent.Append(replaceContent);
+                    fileContent.Append("\r\n");
                 }
+
+                // 書き換え前後で変更がない場合は何もせずに終了する。
+                if (!hasChange) return;
+
+                // 末尾までファイルを読み込み、書き換え用に結合する。
                 while (!streamReader.EndOfStream)
                 {
                     var line = streamReader.ReadLine();
-
+                    if (line == regionEndMarker)
+                    {
+                        innerRegion = false;
+                    }
                     if (!innerRegion)
                     {
                         fileContent.Append(line);
-                        fileContent.Append("\n");
-                    }
-                    else if (line == "#endregion")
-                    {
-                        innerRegion = false;
-                        fileContent.Append(line);
-                        fileContent.Append("\n");
+                        fileContent.Append("\r\n");
                     }
                 }
-                
-                fs.Position = insertPosition;
-                fs.SetLength(insertPosition);
+
+                // 書き換え後の内容でファイルを上書きする。
+                // BOMのため3byte飛ばす。
+                fs.Position = 3;
+                fs.SetLength(3);
                 var streamWriter = new StreamWriter(fs);
                 streamWriter.Write(fileContent);
-                streamWriter.Flush();
-                
+                streamWriter.Flush();                
             }
         }
 
